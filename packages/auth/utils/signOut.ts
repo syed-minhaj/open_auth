448a953a8996f190254f897ad55d7dcafb03e6ff
Minhaj <minhajsulmuneeb@gmail.com>
2025-07-19 15:48:34 +0500

    removing email from cookie after signout

--- packages/auth/utils/signOut.ts
+++ packages/auth/utils/signOut.ts
@@ -3,5 +3,6 @@
 export function signOut() {
     
     document.cookie = `open_auth_jwt=;expires=Tue, 01 Apr 2025 00:00:00 GMT;`;
+    document.cookie = `open_auth_email=;expires=Tue, 01 Apr 2025 00:00:00 GMT;`;
     return {message : "signed out"}
 }
